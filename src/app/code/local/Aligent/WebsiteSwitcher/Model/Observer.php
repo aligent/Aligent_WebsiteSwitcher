6883813719a424dc5a58f9fc039f2a1cfa48a83a
Jim O'Halloran <jim@aligent.com.au>
2013-11-27 12:16:53 +1030

    Minor refactor and tidy up

--- src/app/code/local/Aligent/WebsiteSwitcher/Model/Observer.php
+++ src/app/code/local/Aligent/WebsiteSwitcher/Model/Observer.php
@@ -6,9 +6,13 @@
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *
  * @author 		Luke Mills <luke@aligent.com.au>
+ * @author      Swapna Palaniswamy <swapna@aligent.com.au>
+ * @author      Jim O'Halloran <jim@aligent.com.au>
  */
 class Aligent_WebsiteSwitcher_Model_Observer
 {
+
+    const HANDLE_PREFIX = 'aligent_websiteswitcher_';
 
     public function setStoreCookie() {
         Mage::app()->getCookie()->set(Mage_Core_Model_Store::COOKIE_NAME, Mage::app()->getStore()->getId(), true);
@@ -19,34 +23,24 @@ class Aligent_WebsiteSwitcher_Model_Observer
      */
     public function setLayoutHandles() {
 
-        /** @var $helper Aligent_WebsiteSwitcher_Helper_Data */
-        $helper = Mage::helper('aligent_websiteswitcher');
+        /** @var $oHelper Aligent_WebsiteSwitcher_Helper_Data */
+        $oHelper = Mage::helper('aligent_websiteswitcher');
 
-        /** @var $update Mage_Core_Model_Layout_Update */
-        $update = Mage::app()->getLayout()->getUpdate();
-
-        $handlePrefix = 'aligent_websiteswitcher_';
+        /** @var $oUpdate Mage_Core_Model_Layout_Update */
+        $oUpdate = Mage::app()->getLayout()->getUpdate();
 
         if (count(Mage::app()->getStores()) > 1) {
 
-            if ($helper->canDisplayInMenu()) {
-                $update->addHandle($handlePrefix . 'display_in_menu');
-
+            if ($oHelper->canDisplayInMenu()) {
+                $oUpdate->addHandle(self::HANDLE_PREFIX . 'display_in_menu');
             }
 
-            if ($helper->canDisplayModal()) {
-                $modalDisplayed='';
-                $modalDisplayed= Mage::getSingleton('core/session')->getModalDisplayed();
-                if($modalDisplayed){
-                    return;
-                }
-                else{
-                    $modalDisplayed=true;
-                    Mage::getSingleton('core/session')->setModalDisplayed($modalDisplayed);
-                    $update->addHandle($handlePrefix . 'display_modal');
+            if ($oHelper->canDisplayModal()) {
+                if (!Mage::getSingleton('core/session')->getModalDisplayed()) {
+                    Mage::getSingleton('core/session')->setModalDisplayed(true);
+                    $oUpdate->addHandle(self::HANDLE_PREFIX . 'display_modal');
                 }
             }
-
         }
 
     }
