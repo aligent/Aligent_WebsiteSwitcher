d2e1c4332b0ece15e01bbb5215737380cd4983e7
Jim O'Halloran <jim@aligent.com.au>
2013-11-27 12:23:49 +1030

    Improve docblock

--- src/app/code/local/Aligent/WebsiteSwitcher/Model/Observer.php
+++ src/app/code/local/Aligent/WebsiteSwitcher/Model/Observer.php
@@ -14,12 +14,18 @@ class Aligent_WebsiteSwitcher_Model_Observer
 
     const HANDLE_PREFIX = 'aligent_websiteswitcher_';
 
+    /**
+     * Observe the controller_front_init_before event and set the store cookie
+     * to the current store.
+     */
     public function setStoreCookie() {
         Mage::app()->getCookie()->set(Mage_Core_Model_Store::COOKIE_NAME, Mage::app()->getStore()->getId(), true);
     }
 
+
     /**
-     * Sets appropriate layout handles for the layout system to work with
+     * Observes the controller_action_layout_load_before event amd sets
+     * appropriate layout handles for the layout system to work with.
      */
     public function setLayoutHandles() {
 
