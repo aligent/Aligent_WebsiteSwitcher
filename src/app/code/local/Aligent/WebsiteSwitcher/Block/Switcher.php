5654b3aae9b8c3c98d193f0fb21fd7d30952abfe
Andi Han <andi@aligent.com.au>
2015-06-05 14:56:58 +0930

    adds block cache

--- src/app/code/local/Aligent/WebsiteSwitcher/Block/Switcher.php
+++ src/app/code/local/Aligent/WebsiteSwitcher/Block/Switcher.php
@@ -9,7 +9,20 @@
  */
 class Aligent_WebsiteSwitcher_Block_Switcher extends Mage_Core_Block_Template
 {
-    
+    protected function _construct()
+    {
+        parent::_construct();
+
+        // Add lifetime
+        $this->addData(array('cache_lifetime' => 3600));
+
+        // Add cache tags
+        $this->addCacheTag(array(
+            Mage_Core_Model_Store::CACHE_TAG,
+            Mage_Cms_Model_Block::CACHE_TAG,
+        ));
+    }
+
     public function getStores() {
         return Mage::app()->getStores();
     }
@@ -17,5 +30,15 @@ class Aligent_WebsiteSwitcher_Block_Switcher extends Mage_Core_Block_Template
     public function getCurrentStoreId() {
         return Mage::app()->getStore()->getId();
     }
-    
+
+    public function getCacheKeyInfo()
+    {
+        return array(
+            'ALIGENT_WEBSITE_SWITCHER_BLOCK',
+            Mage::app()->getStore()->getId(),
+            (int)Mage::app()->getStore()->isCurrentlySecure(),
+            Mage::getDesign()->getPackageName(),
+            Mage::getDesign()->getTheme('template')
+        );
+    }
 }
